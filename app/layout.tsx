35bd973f6d3e7324ba364b9a2e26e3304997f9f8
KundaiJonga <kundaiobeyjonga@gmail.com>
2026-03-26 09:40:34 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,21 +21,8 @@ export const metadata: Metadata = {
     siteName: 'Radaverse Solutions',
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/OceanKart LOGO.jpeg',
+    apple: '/OceanKart LOGO.jpeg',
   },
 }
 
